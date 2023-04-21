cad0ed3e1b91de91088d0e1bbd90956403f1dee9
Родионов Алексей Игоревич <rodionov_a@tii.ru>
2023-04-21 13:01:34 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Hex2RgbConverter from './components/Hex2RgbConverter';
+
+function App() {
+  return (
+    <div className="App">
+      <Hex2RgbConverter />
+    </div>
+  );
+}
+
+export default App;
